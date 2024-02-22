786a74693f777b45c789bc6760f095e435bf589b
Narmadaw <narmadaw9@gmail.com>
2024-02-21 21:11:15 -0800

    add breakpoints to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-// import HeroImage from './../../assets/images/hero.jpg'
+import HeroImage from './../../assets/images/hero-1.jpeg'
 
 import './Hero.scss';
 
@@ -6,8 +6,16 @@ const Hero = () =>{
     return(
         <>
         <section className="hero">
-            <div className="hero__side-left"></div>
-            <div className="hero__side-right"></div>
+            <div className="hero__side-left">
+                <img className='hero__image' src={HeroImage} alt="" />
+            </div>
+            <div className="hero__side-right">
+                <div className='hero__text-container'>
+                    <p className='hero__text hero__text--top'>85% would make this again</p>
+                    <h1 className='hero__title'>Mighty Super Cheesecake</h1>
+                    <p className='hero__text hero__text--bottom'>Look no further for a creamy and ultra smooth classic cheesecake recipe! no one can deny its simple decadence.</p>
+                </div>
+            </div>
         </section>
         </>
     )

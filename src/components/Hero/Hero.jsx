dee55b54dd184c7c39f11733e0aa9dedded9804f
Narmadaw <narmadaw9@gmail.com>
2024-02-22 13:09:11 -0800

    breakpoints

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () =>{
             <div className="hero__side-right">
                 <div className='hero__text-container'>
                     <p className='hero__text hero__text--top'>85% would make this again</p>
-                    <h1 className='hero__title'>Mighty Super Cheesecake</h1>
+                    <h4 className='hero__title'>Mighty Super Cheesecake</h4>
                     <p className='hero__text hero__text--bottom'>Look no further for a creamy and ultra smooth classic cheesecake recipe! no one can deny its simple decadence.</p>
                 </div>
             </div>
